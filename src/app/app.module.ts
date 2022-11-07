0821297c69dfe2ae6de7ce6ee1e21624ab0b83f0
Mohamed Lembarki <lembarkimohamed4@gmail.com>
2022-11-07 23:08:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { OrdersListComponent } from './orders-list/orders-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductsListComponent,
+    UsersListComponent,
+    OrdersListComponent,
+    ContactFormComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
