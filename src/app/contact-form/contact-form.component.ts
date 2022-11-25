e4bf461803179fb3c1b8a3d3afe97af4a4239c3e
Mohamed Lembarki <lembarkimohamed4@gmail.com>
2022-11-25 14:02:27 +0100

    finished the contact form. #1

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  email: string = "";
+  message: string = "";
+  displayed_email : string = "";
+  displayed_message : string = "";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+  submit_form(){
+    this.displayed_email = this.email;
+    this.displayed_message = this.message;
+  }
+
+  clearForm(){
+    this.email = "";
+    this.message = "";
   }
 
 }
